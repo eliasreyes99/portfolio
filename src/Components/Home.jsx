ed8f704dc9f82a32a843613b72ffa38510d8690f
eliasreyes99 <139526641+eliasreyes99@users.noreply.github.com>
2024-10-28 22:29:07 -0600

    Add sending...

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,29 +2,46 @@ import "../css/home.css";
 import { LinkedinIcon } from "lucide-react";
 import { GithubIcon } from "lucide-react";
 import SimplePopover from "./SimplePopover";
-import emailjs from 'emailjs-com';
+import emailjs from "emailjs-com";
 import { useRef, useState } from "react";
 import Alerts from "./Alerts";
 export default function Home() {
-  const [alert, setAlert] = useState({ description:"", status:"" });
+  const [alert, setAlert] = useState({ description: "", status: "" });
+  const [sending, setSending] = useState(false);
   const form = useRef();
-  const sendEmail = (e) =>{
+  const sendEmail = (e) => {
     e.preventDefault();
-    emailjs.sendForm('service_3gcha69','template_minf2rk', form.current, 'hRLAM9mKWOrcWw8nF')
-    .then((result) => {
-      console.log(result.text);
-      setAlert({description:'Email sent', status:'alert-success'});
-    },(error) => {
-      console.log(error.text);
-      setAlert({ description:'Error to send email', status:'alert-error' })
-    });
+    setSending(true);
+    emailjs
+      .sendForm(
+        "service_3gcha69",
+        "template_minf2rk",
+        form.current,
+        "hRLAM9mKWOrcWw8nF"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setAlert({ description: "Email sent", status: "alert-success" });
+          form.current.reset();
+        },
+        (error) => {
+          console.log(error.text);
+          setAlert({
+            description: "Error to send email",
+            status: "alert-error",
+          });
+        }
+      )
+      .finally(() => {
+        setSending(false);
+      });
   };
   return (
     <div id="home" className="home">
       {alert.description && (
-        <Alerts description={alert.description}
-        status={alert.status}/>
-      )} 
+        <Alerts description={alert.description} status={alert.status} />
+      )}
       <br />
       <div className="card_presentation">
         <div>
@@ -39,7 +56,8 @@ export default function Home() {
         </div>
       </div>
       <div className="contact">
-        <a title="Linkedin"
+        <a
+          title="Linkedin"
           href="https://www.linkedin.com/in/elias-reyes-gonzalez-873778269/"
           target="_blank"
           rel="noreferrer"
@@ -47,7 +65,7 @@ export default function Home() {
           <LinkedinIcon size={30} className="linkedin" color="#0A66C2" />
         </a>
         <a
-        title="GitHub"
+          title="GitHub"
           href="https://github.com/eliasreyes99"
           target="_blank"
           rel="noreferrer"
@@ -55,7 +73,7 @@ export default function Home() {
           <GithubIcon size={30} className="github" color="#181717" />
         </a>
         <SimplePopover title="Send Mail">
-          <div className="sendMail" >
+          <div className="sendMail">
             <form ref={form} onSubmit={sendEmail}>
               <section>
                 <label>Name</label>
@@ -63,13 +81,15 @@ export default function Home() {
               </section>
               <section>
                 <label>Email</label>
-                <input required type="email" name="user_email"/>
+                <input required type="email" name="user_email" />
               </section>
               <section>
                 <label>Message</label>
                 <textarea required name="message" id="text"></textarea>
               </section>
-              <button type="submit" value="Enviar">Send Mail</button>
+              <button type="submit" value="Enviar">
+                {sending ? "Sending..." : "Send Mail"}
+              </button>
             </form>
           </div>
         </SimplePopover>
